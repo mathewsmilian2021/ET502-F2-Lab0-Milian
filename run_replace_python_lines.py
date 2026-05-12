025ed938691bff7d5f5af2ad81086a95fd5f3c29
mathewsmilian2021 <mathewsmilian2021@gmail.com>
2026-05-12 18:11:25 -0400

    Replace Python with another language when reading lines

---
+++ run_replace_python_lines.py
@@ -0,0 +1,8 @@
+file_path = "earning_python.txt .md"
+language = "C++"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    for line in f:
+        modified = line.replace("Python", language)
+        print(modified, end="")
+
